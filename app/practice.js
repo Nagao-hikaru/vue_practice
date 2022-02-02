285bff2d4cfbd5927a3e7f31b10083547d9ec613
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-02 17:15:05 +0900

    dataオプションの使用方法
    オブジェクト、配列などの使用方法

--- app/practice.js
+++ app/practice.js
@@ -1,6 +1,18 @@
 var app = new Vue({
   el: '#app', //マウントする要素
   data: {
+    // dataオプションはマウント先で使用できるプロパティを定義できる。
     message: 'Hello Vue.js!',
+    count: 10,
+    user: {
+      lastName: 'Yamada',
+      firstName: 'Taro',
+      prefecture: 'Tokyo'
+    },
+    colors: [
+      'Red',
+      'Green',
+      'Blue',
+    ]
   }
 })
