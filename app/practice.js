92d13f566d24f08ce4ac4e2c2ed204b838dd53cd
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-02 17:56:07 +0900

    v-bindの使用方法

--- app/practice.js
+++ app/practice.js
@@ -1,18 +1,8 @@
 var app = new Vue({
   el: '#app', //マウントする要素
   data: {
-    // dataオプションはマウント先で使用できるプロパティを定義できる。
-    message: 'Hello Vue.js!',
-    count: 10,
-    user: {
-      lastName: 'Yamada',
-      firstName: 'Taro',
-      prefecture: 'Tokyo'
-    },
-    colors: [
-      'Red',
-      'Green',
-      'Blue',
-    ]
+    message: 'Hello Vuaae.js',
   }
 })
+
+
