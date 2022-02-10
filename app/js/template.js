b7e907073ad53ee11d6f9d018d249c455f572c68
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-10 11:20:38 +0900

    グローバルフィルタの使い方

--- app/js/template.js
+++ app/js/template.js
@@ -1,13 +1,17 @@
+Vue.filter('numberFormat', function (value) {
+  return value.toLocaleString();
+})
+
 var app = new Vue({
   el: '#app',
   data: {
     price: 2980000,
   },
-  filters: {
-    numberFormat: function (value) {
-      return value.toLocaleString();
-    }
-  },
+  // filters: {
+  //   numberFormat: function (value) {
+  //     return value.toLocaleString();
+  //   }
+  // },
   methods: {
     clickHandler: function (event) {
       this.message = this.message.split('').reverse().join('');
