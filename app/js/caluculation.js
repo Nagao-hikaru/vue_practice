980f83fe608db9c9048fa641f0a6cec6c7f214c3
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-10 20:09:23 +0900

    getter setter設定

--- app/js/caluculation.js
+++ app/js/caluculation.js
@@ -1,11 +1,19 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js'
+    basePrice: 100,
   },
   computed: {
     reversedMessage: function () {
       return this.message.split('').reverse().join('');
+    },
+    taxIncludedPrice: {
+      get: function () {
+        return Math.ceil(this.basePrice * 1.08)
+      },
+      set: function (taxIncludedPrice) {
+        this.basePrice = Math.ceil(taxIncludedPrice / 1.08)
+      }
     }
   }
 })
