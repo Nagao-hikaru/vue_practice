d2c9608dd3f3182061a3383991876e65664dfe89
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-26 17:26:12 +0900

    classを三項演算子で分けれることを確認

--- app/js/class.js
+++ app/js/class.js
@@ -1,8 +1,18 @@
 var app = new Vue({
   el: '#app', //マウントする要素
   data: {
-    items: null,
-    keyWord: '',
-    message: '',
+    isLarge: false,
+    hasError: true,
+    largeClass: {
+      large: true,
+      'bg-gray': true,
+    },
+    dangerClass: {
+      'text-danger': true,
+    },
+    classObject: {
+      large: true,
+      'text-danger': true,
+    },
   },
 })
