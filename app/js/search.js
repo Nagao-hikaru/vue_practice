80576a15610ff4a56145a368df4d4bd03b7dd682
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-26 16:49:01 +0900

    qitta検索サイト作成

--- app/js/search.js
+++ app/js/search.js
@@ -1,15 +1,43 @@
 var app = new Vue({
   el: '#app', //マウントする要素
   data: {
-
+    items: null,
+    keyWord: '',
+    message: '',
   },
   watch: {
-
+    keyWord: function (newKeyWord, oldKeyWord) {
+      // console.log(newKeyWord)
+      this.message = 'waiting for you stop typing'
+      this.debouncedGetAnswer()
+    }
   },
   created: function () {
-
+    // this.keyWord = 'JavaScript'
+    // this.getAnswer()
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
   },
   methods: {
+    getAnswer: function () {
+      if (this.keyWord === '') {
+        this.items = null
+        return 
+      }
 
+      this.message = 'Loading...';
+      var vm = this
+      var params = { page: 1, per_page: 20, query: this.keyWord }
+      axios.get('https://qiita.com/api/v2/items', { params })
+        .then(function (response) {
+          console.log(response);
+          vm.items = response.data
+        })
+        .catch(function (error) {
+          vm.message = 'error message'
+        })
+        .finally(function () {
+          vm.message = ''
+        })
+    }
   },
 })
--- app/realtime_search_start/js/main.js
+++ app/realtime_search_start/js/main.js
@@ -1,3 +1,38 @@
 var app = new Vue({
-    el: '#app'
-})
+    el: '#app', //マウントする要素
+    data: {
+      items: null,
+      keyWord: '',
+      message: '',
+    },
+    watch: {
+  
+    },
+    created: function () {
+      this.keyWord = 'JavaScript'
+      this.getAnswer()
+    },
+    methods: {
+      getAnswer: function () {
+        if (this.keyWord === '') {
+          this.items = null
+          return 
+        }
+  
+        this.message = 'Loading...';
+        var vm = this
+        var params = { page: 1, per_page: 20, query: this.keyWord }
+        axios.get('https://qiita.com/api/v2/items', { params })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            vm.message = 'error message'
+          })
+          .finally(function () {
+            vm.message = ''
+          })
+      }
+    },
+  })
+  
