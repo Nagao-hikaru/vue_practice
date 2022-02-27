f9de4bbdaa3026bf918829293704e9721a415bc1
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-27 13:50:35 +0900

    コンポーネントのdataプロパティの設定

--- app/js/component.js
+++ app/js/component.js
@@ -1,14 +1,22 @@
-// Vue.component('hello-component', {
-//   template: '<p>hello</p>',
-// })
-var helloComponent = {
+Vue.component('hello-component', {
   template: '<p>hello</p>',
-}
+}),
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">{{ count }}</button>'
+})
+// var helloComponent = {
+//   template: '<p>hello</p>',
+// }
 var app = new Vue({
   el: '#app', //マウントする要素
   data: {
   },
-  components: {
-    'hello-component' : helloComponent
-  }
+  // components: {
+  //   'hello-component' : helloComponent //今ポーメント名はケバブケースで書く。
+  // }
 })
