60a8f8173fa1d77bf536892395ebd5f13c9d3d47
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-27 13:42:25 +0900

    コンポーネントのローカル登録

--- app/js/component.js
+++ app/js/component.js
@@ -1,8 +1,14 @@
-Vue.component('hello-component', {
+// Vue.component('hello-component', {
+//   template: '<p>hello</p>',
+// })
+var helloComponent = {
   template: '<p>hello</p>',
-})
+}
 var app = new Vue({
   el: '#app', //マウントする要素
   data: {
   },
+  components: {
+    'hello-component' : helloComponent
+  }
 })
