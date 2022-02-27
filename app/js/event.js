49ad1dc1f0e5c981d6db25c92233ff48079d25db
Nagao-hikaru <koyanagi29sushi@gmail.com>
2022-02-27 12:49:48 +0900

    引数の使用方法

--- app/js/event.js
+++ app/js/event.js
@@ -2,10 +2,13 @@ var app = new Vue({
   el: '#app', //マウントする要素
   data: {
     counter: 0,
+    message: '',
   },
   methods: {
-    clickHandler: function (dd) {
+    clickHandler: function ($event, message) {
       this.counter ++
-    }
+      this.message = message
+    },
+    
   },
 })
